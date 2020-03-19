75f813d00ebe0b4f2ce6710a5adfc5e63af58759
Jhin Scripter <sst.js6h@gmail.com>
2020-03-19 13:24:18 +0000

    useless
    fix

--- writeups/algoritmo/Str-ROT.py
+++ writeups/algoritmo/Str-ROT.py
@@ -2,4 +2,4 @@ texto = input("Texto: ")
 rot = int(input("ROT: "))
 
 print("Esquerda: " + texto[rot:] + texto[:rot])
-print("Direita: " + texto[rot+1] + texto[0:rot+1])
+print("Direita: " + texto[rot+1] + texto[:rot+1])
